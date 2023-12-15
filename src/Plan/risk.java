5c0f6a59b241133b3f0153ce0391d0a78bde5fd7
Saurabh <saurabhbisht717@gmail.com>
2023-12-15 13:12:51 +0530

    initial commit

---
+++ src/Plan/risk.java
@@ -0,0 +1,14 @@
+// Version: 1.0
+// Plan: Wang Zexue, Yuchen Liu, Kunlun Wang, Yixiong Ding
+// Date: 2018/4/15
+// Description: This is the main class of the project. It will print "Hello, World!" on the screen.
+
+package Plan;
+
+public class risk {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
